4eb07d6d69c828740cd3f5cbea1c6ce294c47380
Paul Wooten <Paul.Wooten@hyland.com>
2021-05-13 08:59:55 -0400

    Commit changes for Authentication

--- CleanArch/CleanArch.Mvc/Controllers/CourseController.cs
+++ CleanArch/CleanArch.Mvc/Controllers/CourseController.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using CleanArch.Application.Interfaces;
 using CleanArch.Application.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CleanArch.Mvc.Controllers
 {
+    [Authorize]
     public class CourseController : Controller
     {
         private ICourseService _courseService;

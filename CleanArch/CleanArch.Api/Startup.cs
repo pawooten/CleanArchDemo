b2b03ba973fee42dc17584d5409c061fe3473417
Paul Wooten <Paul.Wooten@hyland.com>
2021-05-17 19:49:35 -0400

    Swagger doc

--- CleanArch/CleanArch.Api/Startup.cs
+++ CleanArch/CleanArch.Api/Startup.cs
@@ -2,14 +2,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CleanArch.Infra.Data.Context;
+using CleanArch.Infra.IoC;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace CleanArch.Api
 {
@@ -25,7 +30,18 @@ namespace CleanArch.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices( IServiceCollection services )
         {
+            services.AddDbContext<UniversityDBContext>( options =>
+            {
+                options.UseSqlServer( Configuration.GetConnectionString( "UniversityDBConnection" ) );
+            } );
+
             services.AddControllers();
+            services.AddMediatR( typeof( Startup ) );
+            services.AddSwaggerGen( c =>
+             {
+                 c.SwaggerDoc( "v1", new OpenApiInfo { Title = "University Api", Version = "v1" } );
+             } );
+            RegisterServices( services );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +62,16 @@ namespace CleanArch.Api
              {
                  endpoints.MapControllers();
              } );
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint( "/swagger/v1/swagger.json", "University Api V1" );
+            } );
+        }
+
+        private static void RegisterServices( IServiceCollection services )
+        {
+            DependencyContainer.RegisterServices( services );
         }
     }
 }
--- CleanArch/CleanArch.Mvc/Startup.cs
+++ CleanArch/CleanArch.Mvc/Startup.cs
@@ -14,6 +14,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using CleanArch.Infra.Data.Context;
 using CleanArch.Infra.IoC;
+using MediatR;
 
 namespace CleanArch.Mvc
 {
@@ -40,7 +41,7 @@ namespace CleanArch.Mvc
             } );
             services.AddControllersWithViews();
             services.AddRazorPages();
-
+            services.AddMediatR( typeof( Startup ) );
             RegisterServices(services);
         }
 
